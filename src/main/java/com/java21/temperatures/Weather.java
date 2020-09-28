6290f4a7bd9b8b07227fbfb29328156809b44fb2
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-28 21:27:50 +0200

    Java21 day2 - commit 2

--- src/main/java/com/java21/temperatures/Weather.java
+++ src/main/java/com/java21/temperatures/Weather.java
@@ -3,15 +3,15 @@ package com.java21.temperatures;
 public class Weather {
     public static void main(String[] args){
         float fah = 86;
-        System.out.println(fah + " Fahrenheit degrees");
+        System.out.println(fah + " Fahrenheit degrees is ");
         //convert Fahrenheit to Celsius
         fah = fah - 32;
         fah = fah / 9;
         fah = fah * 5;
-        System.out.println(fah + " Celsius degrees");
+        System.out.println(fah + " Celsius degrees\n");
 
         float cel = 33;
-        System.out.println(cel + " Celsius degrees");
+        System.out.println(cel + " Celsius degrees is ");
         //convert Celsius to Fahrenheit
         cel = cel * 9;
         cel = cel / 5;
