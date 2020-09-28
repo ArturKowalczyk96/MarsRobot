a18a720ea38644c1a61fb310cd48faeb9bf9b39d
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-28 22:17:04 +0200

    Java21 day2 - commit 4

---
+++ src/main/java/com/java21/tasks/d2/t2/Operators.java
@@ -0,0 +1,13 @@
+package com.java21.tasks.d2.t2;
+
+public class Operators {
+    public static void main(String[] args){
+        double no1 = 321;
+        double no2 = 43;
+        double res1;
+        double res2;
+        res1 = no1 / no2;
+        res2 = no1 % no2;
+        System.out.println("From given numbers: " + no1 + " " + no2 + ". Division is: " + res1 + "\tand the rest of the division is: " + res2);
+    }
+}
