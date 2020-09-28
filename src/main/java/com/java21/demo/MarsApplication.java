de1363bad90adf06c72df09053a349cb647fcde2
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-28 21:25:58 +0200

    Java21 day2 - commit 1

--- src/main/java/com/java21/demo/MarsApplication.java
+++ src/main/java/com/java21/demo/MarsApplication.java
@@ -9,10 +9,9 @@ public class MarsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarsApplication.class, args);
         MarsRobot spirit = new MarsRobot();
-        spirit.status = "\nExploration";
+        spirit.status = "\nSpirit: Exploration";
         spirit.speed = 2;
         spirit.temperature = -60;
-
         spirit.showAttributes();
         System.out.println("\nChange spped to 3");
         spirit.speed = 3;
@@ -23,6 +22,18 @@ public class MarsApplication {
         System.out.println("\nCheck temperature");
         spirit.checkTemperature();
         spirit.showAttributes();
+
+        MarsRobot opportunity = new MarsRobot();
+        opportunity.status = "\nOpportunity: Exploration";
+        opportunity.speed = 3;
+        opportunity.temperature = -50;
+        opportunity.showAttributes();
+        System.out.println("\nChange of temperature to -95");
+        opportunity.temperature = -95;
+        opportunity.showAttributes();
+        System.out.println("Check temperature");
+        opportunity.checkTemperature();
+        opportunity.showAttributes();
     }
 
 }
---
+++ src/main/java/com/java21/temperatures/Weather.java
@@ -0,0 +1,21 @@
+package com.java21.temperatures;
+
+public class Weather {
+    public static void main(String[] args){
+        float fah = 86;
+        System.out.println(fah + " Fahrenheit degrees");
+        //convert Fahrenheit to Celsius
+        fah = fah - 32;
+        fah = fah / 9;
+        fah = fah * 5;
+        System.out.println(fah + " Celsius degrees");
+
+        float cel = 33;
+        System.out.println(cel + " Celsius degrees");
+        //convert Celsius to Fahrenheit
+        cel = cel * 9;
+        cel = cel / 5;
+        cel = cel + 32;
+        System.out.println(cel + " Fahrenheit degrees");
+    }
+}
