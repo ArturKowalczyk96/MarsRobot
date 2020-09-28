56963f3be8b282f9bcac86c6eac1e48ef0d667f6
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2020-09-28 22:59:37 +0200

    Java21 day3 - commit 3

--- src/main/java/com/java21/tasks/d3/StringChecker.java
+++ src/main/java/com/java21/tasks/d3/StringChecker.java
@@ -1,4 +1,20 @@
 package com.java21.tasks.d3;
 
 public class StringChecker {
+    public static void main(String[] args) {
+        String str = "Lannister always pays his debts.";
+        System.out.println("Text is: " + str);
+        System.out.println("Length of text: "
+                + str.length());
+        System.out.println("Char on position 6.: "
+                + str.charAt(6));
+        System.out.println("Fragment from char 10. to 16.: "
+                + str.substring(10, 16));
+        System.out.println("Index of first letter 't': "
+                + str.indexOf('t'));
+        System.out.println("Index begining of subtext "
+                + "\"debts\": " + str.indexOf("debts"));
+        System.out.println("Text written in capital letters: "
+                + str.toUpperCase());
+    }
 }
